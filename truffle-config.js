1c255dc5e89fc73e10f8bcec58468aeb1f42fb1e
0xawaz <amina.waddiz@gmail.com>
2024-08-05 12:47:33 +0200

    update solidity to 0.8.20

--- truffle-config.js
+++ truffle-config.js
@@ -6,14 +6,14 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*", // Match any network id
+      network_id: "*",
     },
     sepolia: {
       provider: () => new HDWalletProvider(
         process.env.MNEMONIC,
         `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
       ),
-      network_id: 11155111, // Sepolia network id
+      network_id: 11155111,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
@@ -21,7 +21,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.20",
     },
   },
 };
