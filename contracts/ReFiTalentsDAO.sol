4c288485ad66f5ccb0e8cb27e1f49905051913cf
0xawaz <amina.waddiz@gmail.com>
2024-08-05 12:30:31 +0200

    add dynamic values to smart contract

--- contracts/ReFiTalentsDAO.sol
+++ contracts/ReFiTalentsDAO.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ReFiTalentsDAO is ERC20, Ownable {
-    constructor() ERC20("ReFi Talents Token", "RFT") {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
